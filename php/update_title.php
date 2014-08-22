963da541f895f3e492398ed02841232782d1bf63
Oscar Francisco Ramírez Andalón <ofra.2925@gmail.com>
2014-08-21 19:05:35 -0600

    archivo update_title.php Eliminar mysql deprecate cambiandolo por mysqli

--- php/update_title.php
+++ php/update_title.php
@@ -3,18 +3,18 @@ include ('conexion.php');
 
 $title = $_POST['title'];
 $id = $_POST['id'];
-$result2 = mysql_query("SELECT * FROM Album WHERE Titulo_album='$title'")or die("Problemas enla consulta: ".mysql_error());
+$result2 = mysqli_query($con,"SELECT * FROM Album WHERE Titulo_album='$title'")or die("Problemas enla consulta: ".mysqli_error());
 
-$total = mysql_num_rows($result2);  
+$total = mysqli_num_rows($result2);  
 if($total==0){//valida que El album no exista 
-	$result1 = mysql_query("UPDATE Album SET Titulo_album='$title' WHERE Id_album ='$id'")or die("Problemas enla consulta: ".mysql_error());  
+	$result1 = mysqli_query($con,"UPDATE Album SET Titulo_album='$title' WHERE Id_album ='$id'")or die("Problemas enla consulta: ".mysqli_error());  
 
             if (!$result1){
-                    echo 'La consulta SQL contiene errores.'.mysql_error()."\n";
+                    echo 'La consulta SQL contiene errores.'.mysqli_error()."\n";
                 }else {
                     echo "Los cambios se Guardaron Correctamente.";
                 }
-        mysql_close($con);
+        mysqli_close($con);
 
 }else{
     echo "El Album ya existe, intente con otro nombre.";
