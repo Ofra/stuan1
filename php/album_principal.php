13667583e8fba3d562b05b193a6e05a99f53f413
Oscar Francisco Ramírez Andalón <ofra.2925@gmail.com>
2014-08-21 18:48:24 -0600

    archivo album_principal.php Eliminar mysql deprecate cambiandolo por mysqli

--- php/album_principal.php
+++ php/album_principal.php
@@ -2,22 +2,22 @@
 	include 'conexion.php';
 	$id = $_POST['id_album'];
 
-	$result0 = mysql_query("SELECT Id_album From Album WHERE Principal ='1'")or die("Problemas enla consulta: ".mysql_error());
+	$result0 = mysqli_query($con,"SELECT Id_album From Album WHERE Principal ='1'")or die("Problemas enla consulta: ".mysqli_error());
 
-	while($reg0 = mysql_fetch_array($result0)){	
+	while($reg0 = mysqli_fetch_array($result0)){	
 			$Id_album_viejo = $reg0['Id_album'];
 		}
 
-	$result2 = mysql_query("UPDATE Album SET Principal='0' WHERE Id_album ='$Id_album_viejo'")or die("Problemas enla consulta: ".mysql_error());
+	$result2 = mysqli_query($con,"UPDATE Album SET Principal='0' WHERE Id_album ='$Id_album_viejo'")or die("Problemas enla consulta: ".mysqli_error());
 
-	$result = mysql_query("UPDATE Album SET Principal='1' WHERE Id_album ='$id'")or die("Problemas enla consulta: ".mysql_error());
+	$result = mysqli_query($con,"UPDATE Album SET Principal='1' WHERE Id_album ='$id'")or die("Problemas enla consulta: ".mysqli_error());
 	
 	if(!$result){
-		echo 'La consulta SQL contiene errores.'.mysql_error()."\n";
+		echo 'La consulta SQL contiene errores.'.mysqli_error()."\n";
 	}else{
-		$result1 = mysql_query("SELECT Titulo_album FROM Album WHERE Id_album ='$id'")or die("Problemas enla consulta: ".mysql_error());
+		$result1 = mysqli_query($con,"SELECT Titulo_album FROM Album WHERE Id_album ='$id'")or die("Problemas enla consulta: ".mysqli_error());
 		
-		while($reg = mysql_fetch_array($result1)){	
+		while($reg = mysqli_fetch_array($result1)){	
 			$title = $reg['Titulo_album'];
 		}
 
